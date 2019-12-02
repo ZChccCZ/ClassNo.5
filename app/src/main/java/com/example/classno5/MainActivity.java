c29042b479e1d38c06e3c7396ffc3a9c62d52425
ZC <2508035263@qq.com>
2019-12-02 22:05:11 +0800

    增加网络图片加载方式

--- app/src/main/java/com/example/classno5/MainActivity.java
+++ app/src/main/java/com/example/classno5/MainActivity.java
@@ -2,11 +2,16 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
@@ -23,11 +28,18 @@ import okhttp3.Response;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=findViewById(R.id.text);
+        imageView=findViewById(R.id.image);
+        //运用glide框架加载图片
+//        Glide.with(this).load("https://avatars2.githubusercontent.com/u/14975630?v=4&s=120").into(imageView);
+        //自定义的获取图片的方法，只能加载部分图片，有些链接不支持
+        //思考：怎样将获取网络图片的代码完善
+        getP();
         getDataAsync();
 
     }
@@ -44,6 +56,12 @@ public class MainActivity extends AppCompatActivity {
 
                 getJsonString(responseBody);
                 //textView.setText(responseBody);
+            }
+            if(msg.what==2){
+                Bundle data=msg.getData();
+                byte[] bytes= data.getByteArray("bytes");
+                Bitmap myPhoto= BitmapFactory.decodeByteArray(bytes,0,bytes.length);
+                imageView.setImageBitmap(myPhoto);
             }
         }
     };
@@ -67,12 +85,7 @@ public class MainActivity extends AppCompatActivity {
         }catch (Exception e){
             e.printStackTrace();
         }
-
-
-
     }
-
-
 
     public void getDataAsync(){
         //创建client
@@ -110,8 +123,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
     }
+    public void getP(){
+        //创建client
+        final OkHttpClient client=new OkHttpClient();
+        //创建request
+        final Request request=new Request.Builder().url("https://cn.bing.com/sa/simg/hpb/LaDigue_EN-CA1115245085_1920x1080.jpg").build();
+        //发起请求
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                //打印失败日志
+                Log.d("failed","请求网络错误");
+            }
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                //获取responseBody
+                String responseBody;
+                if(response.isSuccessful()){
 
+                    byte[] bytes=response.body().bytes();
+
+                    //创建message
+                    Message msg=Message.obtain();
+                    //创建bundle
+                    Bundle data=new Bundle();
+                    //将responseBody放进data中
+                    data.putByteArray("bytes",bytes);
+                    //将data放进msg中
+                    msg.setData(data);
+                    //给msg设定一个id
+                    msg.what=2;
+                    //将msg传递给handler
+                    handler.sendMessage(msg);
+                }else {
+                    Log.d("failed","处理图片错误");
+                }
+            }
+        });
+    }
 }
